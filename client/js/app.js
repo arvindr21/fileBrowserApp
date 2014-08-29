f687cd0f03b80a1f00f3576a6fba00889beb17e4
arvindr21 <arvind.ravulavaru@gmail.com>
2014-08-29 09:09:40 +0530

    fixed inconsistencies

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,18 @@
-var app = angular.module('fileBrowserApp', ['ngRoute', 'jsTree.directive']).
-config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-    when('/', {
-      templateUrl: '../partials/home.html',
-      controller: 'HomeCtrl'
-    }).
-    otherwise({
-      redirectTo: '/home'
-    });
-  }
-]);
+(function() {
+  'use strict';
+
+  window.app = angular.module('fileBrowserApp', ['ngRoute', 'jsTree.directive']).
+  config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+      when('/', {
+        templateUrl: '../partials/home.html',
+        controller: 'HomeCtrl'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+    }
+  ]);
+
+}());
--- client/js/factory.js
+++ client/js/factory.js
@@ -1,9 +1,16 @@
-app.factory('FetchFileFactory', function($http) {
-  var _factory = {};
-  
-  _factory.fetchFile = function(file) {
-    return $http.get('/api/resource?resource=' + encodeURIComponent(file));
-  };
-  
-  return _factory;
-});
+(function() {
+  'use strict';
+
+  app.factory('FetchFileFactory', ['$http',
+    function($http) {
+      var _factory = {};
+
+      _factory.fetchFile = function(file) {
+        return $http.get('/api/resource?resource=' + encodeURIComponent(file));
+      };
+
+      return _factory;
+    }
+  ]);
+
+}());
